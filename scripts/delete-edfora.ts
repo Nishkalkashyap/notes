24400693b2c02cd46fec035fd3048382fa3c5b23
Nishkal <kashyapnishkal@gmail.com>
2019-11-13 14:02:10 +0530

    updated delete edfora script

--- scripts/delete-edfora.ts
+++ scripts/delete-edfora.ts
@@ -1,2 +1,6 @@
 import * as fs from 'fs-extra';
+
+console.log(fs.readdirSync('./.vuepress/dist/edfora'));
 fs.emptyDirSync('./.vuepress/dist/edfora');
+console.log(fs.readdirSync('./.vuepress/dist/edfora'));
+fs.rmdirSync('./.vuepress/dist/edfora');
