1e9b353eb2e5327c72209d491acb9b4be4f8c531
Nishkal <kashyapnishkal@gmail.com>
2019-11-19 11:40:16 +0530

    updated pre-build script

--- scripts/delete-edfora.ts
+++ scripts/delete-edfora.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs-extra';
 
-console.log(fs.readdirSync('./.vuepress/dist/edfora'));
-fs.emptyDirSync('./.vuepress/dist/edfora');
-console.log(fs.readdirSync('./.vuepress/dist/edfora'));
-fs.rmdirSync('./.vuepress/dist/edfora');
+// fs.emptyDirSync('./.vuepress/dist/edfora');
+// fs.rmdirSync('./.vuepress/dist/edfora');
+
+fs.emptyDirSync('./edfora');
+fs.rmdirSync('./edfora');
